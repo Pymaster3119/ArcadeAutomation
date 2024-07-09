1d5d2811c81e9a3dc2518b4cc4d41c6179d090e6
pymaster9 <adi31tya@icloud.com>
2024-07-08 20:29:10 -0400

    Finished code to commit to GitHub

--- Arcade.py
+++ Arcade.py
@@ -1,6 +1,9 @@
 from tkinter import *
 import math
+import os
+import pygame
 
+pygame.mixer.init()
 tk = Tk("Arcade Automation - GIT Management, Slack calls, Timer")
 tk.geometry("500x500")
 frame = Frame(tk, width=500, height=500)
@@ -8,6 +11,7 @@ frame.pack()
 sessionDescription = StringVar(tk)
 remoteOrigin = StringVar(tk)
 timeRemaining = StringVar(tk)
+directory = StringVar(tk)
 secondsRemaining = 10#3600
 
 #Main Menu
@@ -22,7 +26,9 @@ def drawStartSession():
     Entry(frame, textvariable=sessionDescription).grid(row = 0, column= 1)
     Label(frame, text= "Enter your GitHub Repo's remote origin:").grid(row=1, column=0)
     Entry(frame, textvariable=remoteOrigin).grid(row = 1, column= 1)
-    Button(frame, text= "Start Session!", command=drawTimer).grid(row = 2, column= 0, columnspan = 2, sticky = W+E)
+    Label(frame, text= "Enter your files' parent directory").grid(row=2, column=0)
+    Entry(frame, textvariable=directory).grid(row = 2, column= 1)
+    Button(frame, text= "Start Session!", command=drawTimer).grid(row = 3, column= 0, columnspan = 2, sticky = W+E)
 
 def drawTimer():
     for widget in frame.winfo_children():
@@ -43,9 +49,32 @@ def updateTimer():
         endSession()
 
 def endSession():
+    #Git commit
+    os.system("git init")
+    searchPath(directory.get())
+    os.system("git commit -m \"" + sessionDescription.get() + "\"")
+    os.system("git branch -M main")
+    os.system("git remote add origin " + remoteOrigin.get())
+    os.system("git push -u origin main")
+
+    
     #Upload stuff to Slack and git commit, etc
     drawStartSession()
     
+def searchPath(pathname):
+    if "/." in pathname:
+        return
+    for file in os.listdir(path=pathname):
+        if os.path.isfile(file):
+            if os.path.getsize(file) < 100000000:
+                os.system(f"git add " + os.path.realpath(file))
+            else:
+                playSound('fileTooLarge.mp3')
+        else:
+            searchPath(os.path.realpath(file))
 
+def playSound(sound):
+    pygame.mixer.music.load(os.getcwd() + "/" + sound)
+    pygame.mixer.music.play(loops=0)
 drawMainMenu()
 tk.mainloop()
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # ArcadeAutomation
+This was created to automate the commit process for Arcade and automatically commits code to GitHub and runs slack commands for users every hour.
+To commit, I use the command line and the following commands:
+    git init
+    (for each file less than 100MB) git add <filename>
+    git commit -m "<user message>"
+    git branch -M main
+    git remote add origin <origin>
+    git push -u origin main
+
+Program assumes you have pip/pip3 to install all libraries. Also, /. makes program ignore that path.
